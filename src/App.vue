3bd0db3d5a3cf40dc3c542235f1c59beb6ec0ff7
nagai0829 <ke.seiya0829@gmail.com>
2021-02-23 20:43:47 +0900

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <button @click="result">結果を見る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#app',
+  data() {
+    return {
+      list: [
+        '大吉',
+        '中吉',
+        '小吉',
+        '末吉'
+      ],
+    }
+  },
+  methods: {
+    result: function() {
+      const num = Math.floor(Math.random() * this.list.length)
+      alert(this.list[num])
+    }
+  }
+};
+</script>
+
+
